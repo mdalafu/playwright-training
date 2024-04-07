54f4bd8c089583cdc46ea6ae93cae9e501a526d6
luisadalafu <luisa.dalafu@waterservicesreform.govt.nz>
2024-04-08 10:29:36 +1200

    add module2 exercise

---
+++ module1/1_exercise.ts
@@ -0,0 +1,12 @@
+
+function calculateSquare(num: number): number{
+    return num * num;
+}
+
+console.log(calculateSquare(5));
+
+const calculateTriangleArea = (base:number, height: number):number =>
+    (height * base) / 2;
+
+console.log(calculateTriangleArea(2,3));
+  
