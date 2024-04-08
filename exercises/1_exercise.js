93bc854b5d84bbea9c62c9975c041829c4a5ccd8
luisadalafu <luisa.dalafu@waterservicesreform.govt.nz>
2024-04-09 11:48:10 +1200

    delete folder

--- exercises/1_exercise.js
+++
@@ -1,8 +0,0 @@
-function calculateSquare(num) {
-    return num * num;
-}
-console.log(calculateSquare(5));
-var calculateTriangleArea = function (base, height) {
-    return (height * base) / 2;
-};
-console.log(calculateTriangleArea(2, 3));
